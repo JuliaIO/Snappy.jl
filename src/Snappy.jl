cc8ed6d6822f346e0d7a9ebdde15b7c7a0482af1
Kenta Sato <bicycle1885@gmail.com>
2014-12-30 22:20:39 +0900

    add impl

--- src/Snappy.jl
+++ src/Snappy.jl
@@ -1,5 +1,82 @@
 module Snappy
 
-# package code goes here
+export compress, uncompress
+
+const libsnappy = "/usr/local/lib/libsnappy.dylib"
+
+# snappy status
+const SnappyOK = 0
+const SnappyInvalidInput = 1
+const SnappyBufferTooSmall = 2
+
+function snappy_compress(input::Array{Uint8}, compressed::Array{Uint8})
+    ilen = length(input)
+    olen = Csize_t[length(compressed)]
+    status = ccall(
+        (:snappy_compress, libsnappy),
+        Int,
+        (Ptr{Uint8}, Csize_t, Ptr{Uint8}, Ptr{Csize_t}),
+        input, ilen, compressed, olen
+    )
+    olen[1], status
+end
+
+function snappy_uncompress(compressed::Array{Uint8}, uncompressed::Array{Uint8})
+    ilen = length(compressed)
+    olen = Csize_t[length(uncompressed)]
+    status = ccall(
+        (:snappy_uncompress, libsnappy),
+        Int,
+        (Ptr{Uint8}, Csize_t, Ptr{Uint8}, Ptr{Csize_t}),
+        compressed, ilen, uncompressed, olen
+    )
+    olen[1], status
+end
+
+function snappy_max_compressed_length(source_length::Uint)
+    ccall((:snappy_max_compressed_length, libsnappy), Csize_t, (Csize_t,), source_length)
+end
+
+function snappy_validate_compressed_buffer(compressed::Array{Uint8})
+    ilen = length(compressed)
+    ccall((:snappy_validate_compressed_buffer, libsnappy), Int, (Ptr{Uint8}, Csize_t), compressed, ilen)
+end
+
+function snappy_uncompressed_length(compressed::Array{Uint8})
+    len = length(compressed)
+    result = Csize_t[0]
+    status = ccall((:snappy_uncompressed_length, libsnappy), Int, (Ptr{Uint8}, Csize_t, Ptr{Csize_t}), compressed, len, result)
+    result[1], status
+end
+
+# High-level Interfaces
+
+function compress(input::Array{Uint8})
+    ilen = length(input)
+    maxlen = snappy_max_compressed_length(uint(ilen))
+    compressed = Array(Uint8, maxlen)
+    olen, st = snappy_compress(input, compressed)
+    if st != SnappyOK
+        error("compression failed")
+    end
+    resize!(compressed, olen)
+    compressed
+end
+
+function uncompress(input::Array{Uint8})
+    ilen = length(input)
+    explen, st = snappy_uncompressed_length(input)
+    if st != SnappyOK
+        error("faield to guess the length of the uncompressed data (the compressed data may be broken?)")
+    end
+    uncompressed = Array(Uint8, explen)
+    olen, st = snappy_uncompress(input, uncompressed)
+    if st != SnappyOK
+        error("failed to uncompress the data")
+    end
+    @assert explen == olen
+    resize!(uncompressed, olen)
+    uncompressed
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,110 @@
 using Snappy
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+const SnappyOK = Snappy.SnappyOK
+const SnappyInvalidInput = Snappy.SnappyInvalidInput
+const SnappyBufferTooSmall = Snappy.SnappyBufferTooSmall
+
+# Low-level Interfaces
+
+function originals()
+    ["", "\x00", "\x00\x00", "foo", "foobarbaz", "x"^50]
+end
+
+let
+    # Compress & uncompress small data using pre-allocated buffer
+    buffer_size = 100
+    for original in originals()
+        # compress
+        compressed = Array(Uint8, buffer_size)
+        olen, st = Snappy.snappy_compress(original.data, compressed)
+        @test st == SnappyOK
+        resize!(compressed, olen)
+
+        # uncompress
+        uncompressed = Array(Uint8, buffer_size)
+        olen, st = Snappy.snappy_uncompress(compressed, uncompressed)
+        @test st == SnappyOK
+        resize!(uncompressed, olen)
+
+        restored = ASCIIString(uncompressed)
+        @test original == restored
+    end
+end
+
+let
+    # Prepare too small buffer and try to compress and uncompress data
+    original = "orig"
+
+    # prepare too small buffer to compress
+    compressed = Array(Uint8, 1)
+    _, st = Snappy.snappy_compress(original.data, compressed)
+    @test st == SnappyBufferTooSmall
+
+    # now enough buffer size
+    compressed = Array(Uint8, 100)
+    olen, st = Snappy.snappy_compress(original.data, compressed)
+    @test st == SnappyOK
+    resize!(compressed, olen)
+
+    # again the prepared buffer is too small to uncompress
+    uncompressed = Array(Uint8, 1)
+    olen, st = Snappy.snappy_uncompress(compressed, uncompressed)
+    @test st == SnappyBufferTooSmall
+end
+
+let
+    # Break compressed data and detect it
+    original = "orig"
+    compressed = Array(Uint8, 100)
+    olen, st = Snappy.snappy_compress(original.data, compressed)
+    @test st == SnappyOK
+    resize!(compressed, olen)
+    @test Snappy.snappy_validate_compressed_buffer(compressed) == SnappyOK
+    # perturb the compressed buffer
+    compressed[1] = 0x00
+    @test Snappy.snappy_validate_compressed_buffer(compressed) == SnappyInvalidInput
+end
+
+let
+    # Estimate compressed size
+    for original in originals()
+        maxlen = Snappy.snappy_max_compressed_length(uint(length(original)))
+        compressed = Array(Uint8, 100)
+        olen, st = Snappy.snappy_compress(original.data, compressed)
+        @test st == SnappyOK
+        #@show int(olen), int(maxlen)
+        @test olen <= maxlen
+    end
+end
+
+let
+    # Estimate uncompressed size
+    for original in originals()
+        compressed = Array(Uint8, 100)
+        olen, st = Snappy.snappy_compress(original.data, compressed)
+        @test st == SnappyOK
+        resize!(compressed, olen)
+        olen, st = Snappy.snappy_uncompressed_length(compressed)
+        @test st == SnappyOK
+        @test olen == length(original)
+    end
+end
+
+# High-level Interfaces
+
+let
+    srand(2014)
+    for original in map(randstring, 0:100:1000)
+        @test ASCIIString(uncompress(compress(original.data))) == original
+    end
+end
+
+# Large data
+
+let
+    # 128MiB
+    srand(2014)
+    original = randstring(128 * 1024^2)
+    @test uncompress(compress(original.data)) == original.data
+end
