148fdf91c98868e0fe5a7f22fc217c17c6d385da
Kenta Sato <bicycle1885@gmail.com>
2014-12-31 19:53:52 +0900

    add install dependency for Homebrew

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,11 @@
 using BinDeps
 @BinDeps.setup
-library_dependency("libsnappy")
+
+snappy = library_dependency("libsnappy")
+
+@osx_only begin
+    using Homebrew
+    provides(Homebrew.HB, "snappy", snappy, os=:Darwin)
+end
+
+@BinDeps.install
