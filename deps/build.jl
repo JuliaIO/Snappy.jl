fd651f138e906cfc7fe7efa2afd881dd59ad287c
tan <tanmaykm@gmail.com>
2016-01-18 12:13:25 +0530

    fix deps on linux

--- deps/build.jl
+++ deps/build.jl
@@ -3,9 +3,17 @@ using BinDeps
 
 snappy = library_dependency("libsnappy")
 
+# BinDeps issue: fails on sudo prompt
+#provides(AptGet, {
+#    "libsnappy-dev" => snappy
+#})
+
+provides(Sources, URI("https://github.com/google/snappy/releases/download/1.1.3/snappy-1.1.3.tar.gz"), snappy, unpacked_dir="snappy-1.1.3")
+provides(BuildProcess, Autotools(libtarget = Pkg.dir("Snappy") * "/deps/builds/libsnappy/.libs/libsnappy."*BinDeps.shlib_ext), snappy, os=:Unix)
+
 @osx_only begin
     using Homebrew
     provides(Homebrew.HB, "snappy", snappy, os=:Darwin)
 end
 
-@BinDeps.install
+@BinDeps.install Dict(:libsnappy => :libsnappy)
